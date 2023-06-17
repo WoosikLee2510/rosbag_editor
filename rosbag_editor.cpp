7032e13b17f9e635d66268bf882f92399a72da16
Woosik Lee <woosik@udel.edu>
2023-06-17 15:43:39 -0400

    add sensor specific functions

--- rosbag_editor.cpp
+++ rosbag_editor.cpp
@@ -16,11 +16,23 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
+// ROS
 #include <ros/ros.h>
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
 
+// SYSTEM
+#include <Eigen/Eigen>
+
+#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/JointState.h>
+#include <sensor_msgs/CompressedImage.h>
+#include <sensor_msgs/Imu.h>
+
 using namespace std;
+
 int main(int argc, char **argv) {
   // init the system
   ros::init(argc, argv, "rosbag_editor");
@@ -36,33 +48,102 @@ int main(int argc, char **argv) {
   rosbag::Bag source_bag, target_bag;
   source_bag.open(source_bag_path, rosbag::bagmode::Read);
   target_bag.open(target_bag_path, rosbag::bagmode::Write);
+  rosbag::View msgs(source_bag);
 
   // Load edit settings
   double time_start, time_end;
   nh->param<double>("time_start", time_start, time_start);
   nh->param<double>("time_end", time_end, time_end);
-  cout << "[ROSBAG EDIT]::time_start: " << time_start << endl;
-  cout << "[ROSBAG EDIT]::time_end: " << time_end << endl;
-  if (time_end < 0) // make end time inf if not set
-    time_end = FLT_MAX;
+  time_start = (time_start < 0 ? msgs.getBeginTime().toSec(): time_start);
+  time_end = (time_end < 0 ? msgs.getBeginTime().toSec(): time_end);
+  cout << fixed << "[ROSBAG EDIT]::time_start: " << time_start << endl;
+  cout << fixed << "[ROSBAG EDIT]::time_end: " << time_end << endl;
+  bool add_imu, add_camera, add_lidar, add_vicon, add_wheel1, add_wheel2, add_gps, add_rtk;
+  nh->param<bool>("add_imu", add_imu, false);
+  nh->param<bool>("add_camera", add_camera, false);
+  nh->param<bool>("add_lidar", add_lidar, false);
+  nh->param<bool>("add_vicon", add_vicon, false);
+  nh->param<bool>("add_wheel1", add_wheel1, false);
+  nh->param<bool>("add_wheel2", add_wheel2, false);
+  nh->param<bool>("add_gps", add_gps, false);
+  nh->param<bool>("add_rtk", add_rtk, false);
+  cout << "[ROSBAG EDIT]::Saving: ";
+  cout << (add_imu ? "IMU, " : "");
+  cout << (add_camera ? "CAM, " : "");
+  cout << (add_lidar ? "LIDAR, " : "");
+  cout << (add_vicon ? "VICON, " : "");
+  cout << (add_wheel1 ? "WHEEL(Joint), " : "");
+  cout << (add_wheel2 ? "WHEEL(Control), " : "");
+  cout << (add_gps ? "GPS, " : "");
+  cout << (add_rtk ? "RTK, " : "");
+  cout << endl;
 
   // Edit the messages in the source file and save it to the target file
-  rosbag::View view;
-  view.addQuery(source_bag);
-  for (const rosbag::MessageInstance &msg : view) {
+  printf("Saving ros messages to %s...   ", target_bag_path.c_str());
+  for (const rosbag::MessageInstance &msg : msgs) {
     if (!ros::ok() || msg.getTime().toSec() > time_end)
       break;
 
     if(msg.getTime().toSec() < time_start)
       continue;
 
+    printf("\b\b\b%02d%%", (int)(((msg.getTime().toSec() - time_start) * 100)/(time_end - time_start)));
 
+    // VICON
+    if (add_vicon && msg.getTopic() == "/Husky/world") {
+      geometry_msgs::PoseStamped::ConstPtr vicon = msg.instantiate<geometry_msgs::PoseStamped>();
+      assert(vicon != nullptr);
+      target_bag.write("/optitrack", msg.getTime(), msg);
+    }
+    // GPS
+    if (add_gps && msg.getTopic() == "/gps/fix") {
+      sensor_msgs::NavSatFix::ConstPtr gps = msg.instantiate<sensor_msgs::NavSatFix>();
+      assert(gps != nullptr);
+      target_bag.write(msg.getTopic(), msg.getTime(), msg);
+    }
+    // RTK
+    if (add_rtk && msg.getTopic() == "/fix") {
+      sensor_msgs::NavSatFix::ConstPtr rtk = msg.instantiate<sensor_msgs::NavSatFix>();
+      assert(rtk != nullptr);
+      target_bag.write("/rtk/fix", msg.getTime(), msg);
+    }
+    // LiDAR
+    if (add_lidar && msg.getTopic() == "/ouster/points") {
+      sensor_msgs::PointCloud2::ConstPtr ldr = msg.instantiate<sensor_msgs::PointCloud2>();
+      assert(ldr != nullptr);
+      target_bag.write(msg.getTopic(), msg.getTime(), msg);
+    }
+    // Wheel - Jointstatus
+    if (add_wheel1 && msg.getTopic() == "/joint_status") {
+      sensor_msgs::JointState::ConstPtr whl = msg.instantiate<sensor_msgs::JointState>();
+      assert(whl != nullptr);
+      target_bag.write(msg.getTopic(), msg.getTime(), msg);
+    }
+//    // Wheel - Control
+//    if (add_wheel1 && msg.getTopic() == "/joint_status") {
+//      sensor_msgs::JointState::ConstPtr whl = msg.instantiate<sensor_msgs::JointState>();
+//      assert(whl != nullptr);
+//      target_bag.write("/joint_status", msg.getTime(), msg);
+//    }
 
+    // Camera
+    if (add_camera && (msg.getTopic() == "/t265/fisheye1/image_raw/compressed" || msg.getTopic() == "/t265/fisheye2/image_raw/compressed")) {
+      sensor_msgs::CompressedImage::ConstPtr img = msg.instantiate<sensor_msgs::CompressedImage>();
+      assert(img != nullptr);
+      target_bag.write(msg.getTopic(), msg.getTime(), msg);
+    }
 
-
-    target_bag.write(msg.getTopic(), msg.getTime(), msg);
-
+    // IMU
+    if (add_camera && msg.getTopic() == "/t265/imu") {
+      sensor_msgs::Imu::ConstPtr imu = msg.instantiate<sensor_msgs::Imu>();
+      assert(imu != nullptr);
+      target_bag.write(msg.getTopic(), msg.getTime(), msg);
+    }
   }
-
+  printf("\n");
+  // Close the bag files and finish
+  source_bag.close();
+  target_bag.close();
   return true;
 }
+
